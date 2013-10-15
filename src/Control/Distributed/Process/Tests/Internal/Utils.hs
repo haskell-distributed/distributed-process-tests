5d4c8334f428dd3e1fe20f76d828feac98350c41
Mathieu Boespflug <mathieu.boespflug@parsci.com>
2013-10-15 21:12:13 +0200

    Formatting (alignment).

--- src/Control/Distributed/Process/Tests/Internal/Utils.hs
+++ src/Control/Distributed/Process/Tests/Internal/Utils.hs
@@ -1,4 +1,4 @@
--- | General testing support. Just @import TestUtils@ in your test module.
+-- | General testing support.
 module Control.Distributed.Process.Tests.Internal.Utils where
 
 #if ! MIN_VERSION_base(4,6,0)
@@ -13,7 +13,7 @@ import Control.Concurrent.MVar
 
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
-import Control.Distributed.Process.Serializable()
+import Control.Distributed.Process.Serializable ()
 import Test.HUnit (Assertion)
 import Test.HUnit.Base (assertBool)
 import Test.Framework (Test, defaultMain)
@@ -41,13 +41,13 @@ testMain builder = do
 -- | A mutable cell containing a test result.
 type TestResult a = MVar a
 
-synchronisedAssertion :: (Eq a)
-                 => String
-                 -> LocalNode
-                 -> a
-                 -> (TestResult a -> Process ())
-                 -> MVar ()
-                 -> Assertion
+synchronisedAssertion :: Eq a
+                      => String
+                      -> LocalNode
+                      -> a
+                      -> (TestResult a -> Process ())
+                      -> MVar ()
+                      -> Assertion
 synchronisedAssertion note localNode expected testProc lock = do
   result <- newEmptyMVar
   _ <- forkProcess localNode $ do
@@ -58,14 +58,18 @@ synchronisedAssertion note localNode expected testProc lock = do
   where acquire lock' = liftIO $ takeMVar lock'
         release lock' = liftIO $ putMVar lock' ()
 
-delayedAssertion :: (Eq a) => String -> LocalNode -> a ->
-                    (TestResult a -> Process ()) -> Assertion
+delayedAssertion :: Eq a
+                 => String
+                 -> LocalNode
+                 -> a
+                 -> (TestResult a -> Process ())
+                 -> Assertion
 delayedAssertion note localNode expected testProc = do
   result <- newEmptyMVar
   _ <- forkProcess localNode $ testProc result
   assertComplete note result expected
 
-assertComplete :: (Eq a) => String -> MVar a -> a -> IO ()
+assertComplete :: Eq a => String -> MVar a -> a -> IO ()
 assertComplete msg mv a = do
   b <- takeMVar mv
   assertBool msg (a == b)
